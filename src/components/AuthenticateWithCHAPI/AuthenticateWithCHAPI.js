74c9d2b1d8da0fb3e3c43bb0f6cf15735e311c7a
Orie Steele <orie@transmute.industries>
2020-04-12 11:00:29 -0500

    minor updates

--- src/components/AuthenticateWithCHAPI/AuthenticateWithCHAPI.js
+++ src/components/AuthenticateWithCHAPI/AuthenticateWithCHAPI.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Typography from '@material-ui/core/Typography'
 import Button from '@material-ui/core/Button';
 import Paper from '@material-ui/core/Paper';
+import { v4 as uuidv4 } from 'uuid';
 
 
 function AuthenticateWithCHAPI({ onDIDAuth }) {
@@ -12,6 +13,7 @@ function AuthenticateWithCHAPI({ onDIDAuth }) {
                 // normally this would be contructed on a web server, and passed to the client, 
                 // which would then pass it to CHAPI.
                 // For this demo, we will contruct it on the client...
+                const challenge = uuidv4();
                 const query = {
                     web: {
                         VerifiablePresentation: {
@@ -21,7 +23,7 @@ function AuthenticateWithCHAPI({ onDIDAuth }) {
                             // a 128-bit randomly generated value encoded as a string (use a UUID);
                             // it will be digitally signed in the authentication proof
                             // that will be attached to the VerifiablePresentation response
-                            challenge: '99612b24-63d9-11ea-b99f-4f66f3e4f81a',
+                            challenge, //: '99612b24-63d9-11ea-b99f-4f66f3e4f81a',
                             // the domain that must be digitally signed in the authentication
                             // proof that will be attached to the VerifiablePresentation
                             // response, identifying the recipient
