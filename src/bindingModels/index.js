13317cc6d6fa658a1f2e3a4046d08ff21caf3063
Orie Steele <orie@transmute.industries>
2020-04-23 12:17:52 -0500

    cleanup

--- src/bindingModels/index.js
+++ src/bindingModels/index.js
@@ -1,9 +1,7 @@
 import CertifiedMillTestReport from './CertifiedMillTestReport.json'
 import UniversityDegreeCredential from './UniversityDegreeCredential.json'
-import ImmunoglobulinDetectionTestCard from './ImmunoglobulinDetectionTestCard.json'
 
 export default {
     'CertifiedMillTestReport': CertifiedMillTestReport,
     'UniversityDegreeCredential': UniversityDegreeCredential,
-    'ImmunoglobulinDetectionTestCard': ImmunoglobulinDetectionTestCard
 }
--- src/components/Example/forms/index.js
+++ src/components/Example/forms/index.js
@@ -1,9 +1,7 @@
 import CertifiedMillTestReport from './CertifiedMillTestReport'
 import UniversityDegreeCredential from './UniversityDegreeCredential'
-import ImmunoglobulinDetectionTestCard from './ImmunoglobulinDetectionTestCard'
 
 export default {
     "CertifiedMillTestReport": CertifiedMillTestReport,
-    "UniversityDegreeCredential": UniversityDegreeCredential,
-    'ImmunoglobulinDetectionTestCard': ImmunoglobulinDetectionTestCard
+    "UniversityDegreeCredential": UniversityDegreeCredential
 }
