edbae53679a613720391cf3e81ee838f5e6fffcc
Orie Steele <orie@transmute.industries>
2020-04-27 18:35:42 -0500

    :rocket:

---
+++ docs/precache-manifest.8010efe0dc452f720f0e1ccf68d87709.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a1c7ddffcef3e37468b47dcc37f85b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a91558a97acce2d39dd",
+    "url": "/static/css/main.9603edeb.chunk.css"
+  },
+  {
+    "revision": "9e0598fde3bf761ab319",
+    "url": "/static/js/2.34957d2f.chunk.js"
+  },
+  {
+    "revision": "0cf7ac5139589932918bf42e2ca26ca9",
+    "url": "/static/js/2.34957d2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a91558a97acce2d39dd",
+    "url": "/static/js/main.63f5ae33.chunk.js"
+  },
+  {
+    "revision": "0facbbc5c8ae8a9b41d7",
+    "url": "/static/js/runtime-main.46d13869.js"
+  },
+  {
+    "revision": "32d398ce4682a3baef5f880415dc00be",
+    "url": "/static/media/digital_bazaar.32d398ce.png"
+  },
+  {
+    "revision": "33b194d8ce06cb2e6794ecb444201773",
+    "url": "/static/media/logo.33b194d8.svg"
+  },
+  {
+    "revision": "fcd44906da51d80c1f1768a3846cad0f",
+    "url": "/static/media/transmute.fcd44906.png"
+  }
+]);
